76f2d027c27fd3d71bd9861819425c91a6d9957d
Florence-l <1070883368@qq.com>
2021-06-09 09:10:27 +0800

    new file:   functional_tests/__init__.py 	renamed:    functional_tests.py -> functional_tests/tests.py 	modified:   lists/models.py 	modified:   lists/templates/home.html 	modified:   lists/tests.py 	modified:   lists/views.py
    modified:   lists/templates/home.html
    	modified:   lists/tests.py
    	modified:   superlists/settings.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,35 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
     
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
-        table=self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text,[row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
+        #She notices the page title and header
         self.assertIn('To-Do',self.browser.title)
         header_test = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_test)
 
-
+        #She is invited to enter
         inputbox=self.browser.find_element_by_id('id_new_item')
+        #She types "Buy peacock feathers"
         inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-       
+        #when she hits enter
+        inputbox.send_keys(Keys.ENTER)       
         time.sleep(1)
         self.check_for_row_in_list_table('1:Buy peacock feathers')
-     #there is 
+
+    
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
@@ -37,10 +37,13 @@ class NewVisitorTest(unittest.TestCase):
 
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
-   
+    #there is 
         self.fail('Finish the test!')
        
-        
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])  
 
-if __name__=='__main__':
-    unittest.main(warnings='ignore')
+# if __name__=='__main__':
+#     unittest.main(warnings='ignore')
