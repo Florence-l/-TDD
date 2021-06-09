46d06b92713f3551389bc842b6e764b2b5bd5365
Florence-l <1070883368@qq.com>
2021-06-09 19:57:30 +0800

    modified:   functional_tests/tests.py 	modified:   lists/templates/list.html 	modified:   lists/urls.py 	modified:   lists/views.py 	modified:   superlists/urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path
 from django.conf.urls import url
 from lists import views
 
-
 urlpatterns = [
-
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
-
-
+	url(r'^new$', views.new_list, name='new_list'),
+	url(r'^(\d+)/$', views.view_list, name='view_list'),
+	url(r'^(\d+)/add_item$', views.add_item, name='add_item')
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item,List
+from lists.models import Item, List
+
 
 def home_page(request):
     # if request.method == 'POST':
@@ -8,21 +9,21 @@ def home_page(request):
     #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
-def view_list(request,list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
 
+def view_list(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
-    list_=List.objects.create()
-    Item.objects.create(text=request.POST['item_text'],list=list_)
-    return redirect(f'/lists/{list_.id}/')
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect(f'/lists/{list_.id}/')
 
 
 
