71a3c3c8b4e98e8d1fe8e49a0fdd6d0a4b53e081
Florence-l <1070883368@qq.com>
2021-06-09 21:48:31 +0800

    set STATIC-ROOT in settings and disable adimin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
-import unittest
+import os
 import time
+
 
 MAX_WAIT = 10
 
@@ -12,7 +13,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-
+        staging_server = os.environ.get('STAGING_SERVER') 
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server 
+    
     def tearDown(self):
         self.browser.quit()
 
